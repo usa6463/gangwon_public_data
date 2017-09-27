1d3592d3427e7e55f9f7f2a44d7437ec85c6a4ab
Insoo Kim <usa6463@pukyong.ac.kr>
2017-09-27 17:43:38 +0900

    can append element to state

--- src/components/resort_info_detail/resort_detail.js
+++ src/components/resort_info_detail/resort_detail.js
@@ -36,7 +36,7 @@ export default class ResortDetail extends React.Component {
                     </Card>
 
                     <Card>
-                        <CardImage source={require('../../assets/images/slope.jpg')} />
+                        
                         <CardTitle title= "날씨" />
                         <CardContent text={this.prop.total.CONTENT}/>
                     </Card>
@@ -68,41 +68,11 @@ export default class ResortDetail extends React.Component {
             'X-Naver-Client-Secret' : "aUDG50tsmD",
         },        
         }).then(response =>{
-            console.log(response._bodyInit)
             let obj = JSON.parse(response._bodyInit);
-            console.log(obj.total);
-            console.log(obj.items[0].link);
             this.setState({
                 img_link: obj.items[0].link
             });
         })
-
-        // let myApiUrl = "http://data.gwd.go.kr/apiservice/734a677953757361387467517772/json/tourdb-tourist_attraction-leisure_sports-kr/1/200";
-        // fetch(`${myApiUrl}`, {  
-        // method: 'GET',
-        // }).then(response =>{
-        //     let obj = JSON.parse(response._bodyInit);
-        //     let row = obj[Object.keys(obj)[0]].row;
-        //     row.map(dict => {
-        //         this.state.resort_name.map(name => {
-        //             if(dict.SUBJECT.search(name) >= 0 ){
-        //                 temp = {
-        //                     name : dict.CONTENT_ID,
-        //                     latlng :{
-        //                         latitude: Number(dict.LAT),
-        //                         longitude: Number(dict.LNG)
-        //                     },
-        //                     title : dict.SUBJECT,
-        //                     description : dict.SMGW_ADDRESS_S,
-        //                 }
-        //                 this.state.markers.push(temp);
-        //             }
-        //         })
-        //     });
-        //     this.setState({
-        //         visible: !this.state.visible
-        //     });
-        // })
     }
 }
 
--- src/components/resort_info_detail/restaurant.js
+++ src/components/resort_info_detail/restaurant.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, } from 'react-native';
+import { Text, View, FlatList, List, StyleSheet, } from 'react-native';
 
 import content_styles from '../../assets/styles/content_style';
 import navi_styles from '../../assets/styles/navi_style'
@@ -15,13 +15,45 @@ export default class Restaurant extends React.Component {
 
     constructor(props){
         super(props);
+        this.prop = this.props.navigation.state.params
+        this.state = {
+            restaurants : [
+            ],
+            visible : true,
+        };
     }
     
     render() {
         return (
             <View>
-                <Text style={{alignItems: 'center'}}> This is Restaurant </Text>
+                {/* <Text> HI {this.state.restaurants[0].SMGW_SUBJECT_S}</Text> */}
+                {this.state.restaurants.map(iter =>(
+                    <Text> Hello</Text>
+                ))}
+                
             </View>
         );
     }
+
+    componentWillMount(){
+        let myApiUrl = "http://data.gwd.go.kr/apiservice/734a677953757361387467517772/json/tourdb-restaurant-korean_food-kr/1/2";
+        fetch(`${myApiUrl}`, {
+            method: 'GET',
+        }).then(response =>{
+            let obj = JSON.parse(response._bodyInit);
+            let row = obj[Object.keys(obj)[0]].row;
+
+            row.map(dict => {
+                var restaurants = this.state.restaurants.slice()
+                restaurants.push(dict)
+                this.setState({ restaurants: restaurants })
+            })
+        });
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+    },
+});
